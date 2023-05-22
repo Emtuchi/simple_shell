6dfdc9ea57ea6c1a773ac65a6ec84754327f85ee
amira <amirasaladin@hotmail.com>
2023-05-22 11:54:39 -0700

    Add files 32015

--- simp.c
+++ simp.c
@@ -26,36 +26,35 @@ int main(void)
 	ssize_t outpt;
 	char **argv = NULL;
 
-	/*if ctrl c is typed, it ignores the SIGINT, which ctrl c sends as command and replaces it with our own function(sigint_stop)*/
 	signal(SIGINT, sigint_stop);
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))/*check if file is in interactive mode*/
+		if (isatty(STDIN_FILENO))
 			write(1, "($) ", 4);
 
 		fflush(stdout);
 		outpt = getline(&input, &size, stdin);
-		if (outpt == -1)/*ctrl d is typed(returns -1)*/
+		if (outpt == -1)
 		{
 			free(input);
-			if (isatty(STDIN_FILENO))/*check if we are in interractive mode, if yes, output \n*/
+			if (isatty(STDIN_FILENO))
 				write(1, "\n", 1);
 
 			exit(1);
 		}
 
-		if (*input == '\n' || *input == '\0')/*if enter key is hit without any string*/
+		if (*input == '\n' || *input == '\0')
 			continue;
 
-		argv = srt_input(input);/*tokenize strings with space as delimiter(function is in a different file*/
-		if (!argv || !argv[0])/*if spaces only were inputed, strtok will return NULL cos no command*/
+		argv = srt_input(input);
+		if (!argv || !argv[0])
 		{
 			_free(argv);
 			continue;
 		}
 
-		execpid(argv, input);/*where my excutions and child processes are*/
+		execpid(argv, input);
 
 		_free(argv);
 	}
@@ -76,34 +75,34 @@ int execpid(char *argv[], char *input)
 {
 	pid_t pid;
 	int parent;
-	char *cmd = NULL, *rubbish = argv[0];/*just incase you typed rubbish*/
+	char *cmd = NULL, *rubbish = argv[0];
 
 	if (!*argv)
 		exit(-1);
 
-	pid = fork();/*create child process*/
+	pid = fork();
 
-	if (pid == -1)/*error in creation*/
+	if (pid == -1)
 	{
 		perror("busted");
 		return (1);
 	}
-	else if (pid == 0)/*if we are in the child process*/
+	else if (pid == 0)
 	{
-		cmd = get_path(argv[0]);/*check path.c file for this function, arrange a path for the first command i.e /bin/ which will to added to your ls to become /bin/ls*/
-		if (cmd == NULL)/*if command isnt executable i.e, you typed rubbish in my shell lol*/
+		cmd = get_path(argv[0]);
+		if (cmd == NULL)
 		{
-			perror(rubbish);/*remember or variable*/
+			perror(rubbish);
 			free(input);
 			_free(argv);
 			exit(EXIT_FAILURE);
 		}
 		else
-			execve(cmd, argv, environ);/*execute command if not rubbish*/
+			execve(cmd, argv, environ);
 	}
 	else
 	{
-		wait(&parent);/*make sure parent process doesnt interfere with child process so make it wait, you could also pass NULL*/
+		wait(&parent);
 		fflush(stdout);
 	}
 
